a1799faa6f205a2581005c0329bbfcaf09704330
milad <milad@gmail.com>
2024-12-07 16:18:58 +0330

    feat: onClick

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-
+import {MouseEvent, useState} from "react";
 function App() {
-    const items =[
+    let items = [
         'New York',
         'San Francisco',
         'Tokyo',
         'London',
         'Paris'
-    ]
+    ];
+    const [selectedIndex, setSelectedIndex] = useState()
+    items = [];
+    const  handleClick=(event: MouseEvent)=>{
+        console.log(event);
+    }
+    const getMessage = () => {
+        return items.length === 0 ? <p>No Item Found</p> : null;
+    }
     return <>
+        <h1>List</h1>
+        {getMessage()}
+        {items.length === 0 && <p>No Item Found</p>}
         <ul className="list-group">
-            {items.map(item=> (
-                <li className="list-group-item" key={item}>{item}</li>
+            {items.map((item , index) => (
+                <li className={setSelectedIndex(index)  ? "list-group-item active" :"list-group-item"}
+                    key={item} onClick={handleClick}>
+                    {item}
+                </li>
             ))}
         </ul>
     </>
