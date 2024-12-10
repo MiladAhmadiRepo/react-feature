8c4855f47e6a7deb1363a83464285b2db14bab42
Milad Ahmadi <69338883+ShowAppStructure@users.noreply.github.com>
2024-12-10 21:58:02 +0330

    fix: update array objects with immer

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import {BsArrowDownRightSquareFill} from "react-icons/bs";
 
 import ListGroup from "./components/ListGroup/ListGroup.tsx";
 import Like from "./components/LikeButton/Like.tsx";
-
+import {Immer, produce} from "immer";
 function App() {
     // const items = [
     //     'New York',
@@ -54,16 +54,32 @@ function App() {
     //     //update
     //     setTags(tags.map((tag) => tag === 'happy' ? 'happiness' : tag));
     // }
-    //-------------------------------------- update array objects ---------------------------------------------------------------
+    // //-------------------------------------- update array objects ---------------------------------------------------------------
+    // const [bugs, setBugs] = useState([
+    //     {id:1 , title:"bug 1",fixed : false},
+    //     {id:2 , title:"bug 2",fixed : true},
+    // ]);
+    // const handleClick = () => {
+    //
+    //     //update
+    //     setBugs(bugs.map((bug) => bug.id === 1 ? {...bug,fixed: true}:bug));
+    // }
+    //-------------------------------------- update array objects with immer ---------------------------------------------------------------
     const [bugs, setBugs] = useState([
         {id:1 , title:"bug 1",fixed : false},
         {id:2 , title:"bug 2",fixed : true},
     ]);
     const handleClick = () => {
-
         //update
-        setBugs(bugs.map((bug) => bug.id === 1 ? {...bug,fixed: true}:bug));
+        setBugs(produce(draft => {
+            const bug=draft.find(bug=>bug.id===1)
+            if(bug) bug.fixed=true;
+        }))
     }
+    return <div>
+          {bugs.map(bug => <p key={bug.id} >{bug.title} {bug.fixed? 'Fixed' : 'New '}</p>)}
+          <button onClick={handleClick}> Click Me</button>
+     </div>
 }
 
 export default App
