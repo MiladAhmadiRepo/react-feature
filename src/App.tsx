17d7d1885da76e3025091b53e7601dea05462d1b
milad <milad@gmail.com>
2024-12-10 12:35:37 +0330

    feat : add react-icons to access many kind icons

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Alert from "./components/Alert.tsx";
 import BaseButton from "./components/BaseButton.tsx";
 import {useState} from "react";
 import ListGroupCss from "./components/ListGroup";
+import { BsArrowDownRightSquareFill } from "react-icons/bs";
+
 import ListGroup from "./components/ListGroup/ListGroup.tsx";
 import "./App.css"
 function App() {
@@ -14,7 +16,9 @@ function App() {
         'London',
         'Paris'
     ];
-    return <h1><ListGroup items={items} heading={"Cities"} onSelectItem={item => 1}></ListGroup></h1>;
+    return < ><BsArrowDownRightSquareFill></BsArrowDownRightSquareFill> <h1>
+            <ListGroup items={items} heading={"Cities"} onSelectItem={item => 1}></ListGroup>
+        </h1></>
     //-------------------------------------- click button ---------------------------------------------------------------
     // const [alertVisible, setAlertVisible] = useState(false);
     // return <div>
