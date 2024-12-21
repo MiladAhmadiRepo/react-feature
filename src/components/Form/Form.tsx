36f4548a8f5e767ac605e7cfa3b75844b570ec93
Milad Ahmadi <69338883+ShowAppStructure@users.noreply.github.com>
2024-12-21 19:58:01 +0330

    fix: add form handleSubmmit

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,19 +1,28 @@
 // @flow
-import * as React from 'react';
-import {FormEvent} from "react";
+import {FormEvent, useRef} from "react";
 
 type Props = {};
 
 export function Form(props?: Props) {
+    const nameRef = useRef<HTMLInputElement>(null);
+    const ageRef = useRef<HTMLInputElement>(null);
+    let person= {name: '', age: 0};
     const handleSubmmit = (event: FormEvent) => {
         event.preventDefault();
-        console.log('form submitted');
+        if(nameRef.current!==null) {
+            person.name = nameRef.current.value;
+        }
+        if(ageRef.current!==null) {
+            person.age = +ageRef.current.value;
+        }
+        console.log(person);
+
     }
     return (
         <form onSubmit={handleSubmmit}>
             <div className="Mb-3">
                 <label htmlFor="name" className="form-label">Name</label>
-                <input id="name" type="text" className="form-control"/>
+                <input id="name" ref={nameRef} type="text" className="form-control"/>
             </div>
             <div className="Mb-3 mt-3">
                 <label htmlFor="age" className="form-label">Age</label>
