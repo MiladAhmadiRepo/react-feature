f2d5fe95f20ef3034d9ed3c7c202e14fbdac0234
Milad Ahmadi <69338883+ShowAppStructure@users.noreply.github.com>
2024-12-27 19:00:33 +0330

    fix: use react-hook-form validation form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,33 +1,35 @@
 // @flow
 import {FormEvent, useRef, useState} from "react";
+import {Field, FieldValue, FieldValues, useForm} from "react-hook-form";
 
 type Props = {};
 
-export function Form(props?: Props) {
-    const [person, setPerson] = useState({
-        name: '',
-        age: 0
-    })
-    const handleSubmmit = (event: FormEvent) => {
-        event.preventDefault();
-        console.log(person);
+interface FormData {
+    name: string;
+    age: number;
+}
 
-    }
+export function Form(props?: Props) {
+
+    const {register, handleSubmit, formState: {errors}} = useForm<FormData>();
+    const onSubmit = (data: FieldValues) => console.log(data.toString())
     return (
-        <form onSubmit={handleSubmmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div className="Mb-3">
                 <label htmlFor="name" className="form-label">Name</label>
-                <input id="name" onChange={(event) => {
-                    setPerson({...person, name: event.target.value})
-                }} type="text" className="form-control" value={person.name}/>
+                <input id="name"  {...register("name", {required: true, minLength: 3})}
+                       type="text" className="form-control"/>
+
+                {
+                    errors.name?.type === 'required' && <p className="text-danger">This field is required</p>}
+                {errors.name?.type === 'minLength' &&
+                    <p className="text-danger">The name must be at least 3 characters</p>}
             </div>
             <div className="Mb-3 mt-3">
                 <label htmlFor="age" className="form-label">Age</label>
                 <input id="age"
-                       onChange={(event) => {
-                           setPerson({...person, age: +event.target.value})
-                       }}
-                       type="number" className="from-control" value={person.age}/>
+                       {...register("age", {min: 18, max: 99})}
+                       type="number" className="from-control"/>
             </div>
             <div className="Mb-3 mt-3">
                 <button className="btn btn-primary" type="submit">Submit</button>
